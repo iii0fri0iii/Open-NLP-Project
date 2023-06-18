5bb3be3009dbe740a1b9536cf417bfbbab24c4f1
OLEKSII ARIASOV <oleksii.ariasov@student.uni-tuebingen.de>
2023-06-18 11:37:37 +0200

    fer

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -12,15 +12,42 @@ public class Frame{
 
         firstTextField.setMaximumSize(new Dimension(200,50));
 
+        Dimension size = new Dimension(95, 30);
+
+        JButton loadButton = new JButton("load");
+        firstButton.setMaximumSize(size);
+        firstButton.addActionListener(new loadButtonHandler());
+
+
+
+
         JPanel panel1=new JPanel();
-        BoxLayout aBoxLayout = new BoxLayout(panel1,BoxLayout.Y_AXIS);
+        BoxLayout aBoxLayout = new BoxLayout(panel1,BoxLayout.X_AXIS);
         panel1.setLayout(aBoxLayout);
 
         panel1.add(Box.createRigidArea(new Dimension(0,5)));
+        panel1.add(loadButton);
         panel1.add(firstTextField);
+
+        JPanel panel2=new JPanel();
+        BoxLayout bBoxLayout = new BoxLayout(panel2,BoxLayout.X_AXIS);
+        panel2.setLayout(bBoxLayout);
+
+        panel2.add(Box.createRigidArea(new Dimension(0,5)));
+
+        JPanel panel3=new JPanel();
+        BoxLayout cBoxLayout = new BoxLayout(panel3,BoxLayout.X_AXIS);
+        panel3.setLayout(cBoxLayout);
+
+        panel3.add(Box.createRigidArea(new Dimension(0,5)));
+
+        JButton firstButton = new JButton("first");
+        frame.getContentPane().add(firstButton);
 
         //This is the common size of the buttons
         Dimension size = new Dimension(95, 30);
+
+
 
 
 
